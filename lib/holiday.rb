6ade3a413475a4389a671d3ef1f8ea71b90eb86d
Adrian Popan <adrian.popan@gmail.com>
2019-06-19 23:06:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -44,7 +44,7 @@ def all_winter_holiday_supplies(holiday_hash)
   
   holiday_hash[:winter].collect do |holiday, supplies|
     supplies
-  end
+  end.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
